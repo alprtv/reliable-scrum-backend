fb84836f8f28145b1c1fa4be2b0d036732ff2e51
ap7s <p7sunny@gmail.com>
2017-11-06 21:53:55 +0600

    updated route /api/v1/kanbantools/boards/:boardId/tasks

--- controllers/kanbantool.js
+++ controllers/kanbantool.js
@@ -3,6 +3,9 @@ const HttpError = require('../error').HttpError;
 const fetchBoards = require('../api').fetchBoards;
 const fetchBoard = require('../api').fetchBoard;
 const fetchTasks = require('../api').fetchTasks;
+const fetchTask = require('../api').fetchTask;
+const normalizeUndoneTask = require('../common').normalizeUndoneTask;
+const normalizeDoneTask = require('../common').normalizeDoneTask;
 
 function ApiException(message) {
    this.message = message;
@@ -85,6 +88,7 @@ app.get('/api/v1/kanbantools/boards/:id', (req, res, next) => {
       payload: board
     });
   }).catch((error) => {
+    console.error('error', error);
     res.json({
       success: false,
       payload: error
@@ -96,16 +100,51 @@ app.get('/api/v1/kanbantools/boards/:id', (req, res, next) => {
 app.get('/api/v1/kanbantools/boards/:boardId/tasks', (req, res, next) => {
   const boardId = 337859;
   //const boardId = req.params.id;
+  const undoneStagesIds = [2558931, 2558933, 2558935, 2558937];
+  const doneStagesIds = [2565921, 2565923, 2558949, 2558959];
+  const startProject = '2017-08-13T01:53:30.000-07:00';
+  const bugStageId = 2558925;
+  const bugCardTypeId = 3958725;
 
-  fetchTasks(boardId).then( kbdata => {
-    const data = kbdata.data;
+  fetchTasks(boardId).then( rawData => {
+    const data = rawData.data;
     if (!data) throw new ApiException('Error fetchBoard');
-    return data;
-  }).then((board) => {
+
+    let undoneTasks = [];
+    let rawDoneTasks = [];
+
+    for (task of data) {
+      if (task.time_estimate >= 0 &&
+          task.workflow_stage_id !== bugStageId &&
+          task.card_type_id !== bugCardTypeId) {
+
+        if (undoneStagesIds.includes(task.workflow_stage_id)) {
+          undoneTasks.push(normalizeUndoneTask(task, startProject));
+        }
+        if (doneStagesIds.includes(task.workflow_stage_id)) {
+          rawDoneTasks.push(task);
+        }
+
+      }
+    }
+
+    return { undoneTasks, rawDoneTasks };
+  }).then( tasks => {
+
+    let doneTasks = [];
+    const fetchDoneTasks = tasks.rawDoneTasks.map( task => {
+      return fetchTask(boardId, task.id);
+    });
+
+    Promise.all(fetchDoneTasks).then( results => {
+      console.log(results);
+    });
+
     res.json({
       success: true,
-      payload: board
+      payload: tasks
     });
+
   }).catch((error) => {
     res.json({
       success: false,
