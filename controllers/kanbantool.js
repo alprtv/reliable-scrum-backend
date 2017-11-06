1f900167145ab6b9f3c2c3e394bf383a609d3141
ap7s <p7sunny@gmail.com>
2017-11-06 21:53:39 +0600

    updated route /api/v1/kanbantools/boards/:id

--- controllers/kanbantool.js
+++ controllers/kanbantool.js
@@ -40,14 +40,44 @@ app.get('/api/v1/kanbantools/boards/:id', (req, res, next) => {
   const boardId = 337859;
   //const boardId = req.params.id;
 
-  fetchBoard(boardId).then( kbdata => {
-    const data = kbdata.data;
+  fetchBoard(boardId).then( rawData => {
+    const data = rawData.data;
     if (!data) throw new ApiException('Error fetchBoard');
+    let stages = [];
+
+    for (stage of data.board.workflow_stages) {
+      const id = stage.id;
+      const parentId = stage.parent_id;
+      let hasChilds = false;
+
+      if (parentId) {
+        let name = stage.name;
+
+        for ( let parentStage of data.board.workflow_stages) {
+          if (parentStage.parent_id === id) {
+            hasChilds = true;
+          }
+          if (parentStage.parent_id && parentStage.id === parentId) {
+            name = parentStage.name + ' / ' + name;
+          }
+        }
+
+        if (!hasChilds) {
+          stages.push ({
+            id,
+            name,
+            parentId,
+          });
+        }
+      }
+    }
+
     return {
       id: data.board.id,
       name: data.board.name,
       description: data.board.description,
       createdAt: data.board.created_at,
+      stages,
     };
   }).then((board) => {
     res.json({
